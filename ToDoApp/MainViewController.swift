27beac94c547bf46e042aad38466e55ecbe40c62
Egor Syrtcov <sev_84@inbox.ru>
2020-02-06 15:40:04 +0300

    Create newProject ToDO

---
+++ ToDoApp/MainViewController.swift
@@ -0,0 +1,21 @@
+//
+//  MainViewController.swift
+//  ToDoApp
+//
+//  Created by Egor Syrtcov on 2/6/20.
+//  Copyright © 2020 Egor Syrtcov. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
+
+
