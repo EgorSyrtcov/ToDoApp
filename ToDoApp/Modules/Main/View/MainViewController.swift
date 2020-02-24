6bb43a43c037b66be86ef7bf37eb18162d27f23c
Egor Syrtcov <sev_84@inbox.ru>
2020-02-24 12:39:24 +0300

    Merge in master

--- ToDoApp/Modules/Main/View/MainViewController.swift
+++ ToDoApp/Modules/Main/View/MainViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import CoreData
 
 enum Properties: String {
     case mainCellId = "MainCellId"
@@ -14,12 +15,24 @@ enum Properties: String {
 }
 
 final class MainViewController: UIViewController {
-   
-    var presenter: MainPresenter!
     
-    var defaultTask: [[Task]]? = []
+    var presenter: MainPresenter!
+   // var defaultTask: [[Task]]? = []
+
+    var fetchResultController: NSFetchedResultsController<Tasks>!
     
     @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        presenter.fetchRequest()
+        fetchResultController.delegate = self
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        tableView.reloadData()
+    }
     
     @IBAction private func addTaskButton(_ sender: UIBarButtonItem) {
         presenter.presentCreateVC()
@@ -29,31 +42,49 @@ final class MainViewController: UIViewController {
 extension MainViewController: UITableViewDelegate, UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
+        //return defaultTask?.count ?? 0
 
-        return defaultTask?.count ?? 0
+        return fetchResultController.sections?.count ?? 0
     }
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        let sectionInfo = fetchResultController.sections?[section]
         
-        return defaultTask?[section].count ?? 0
-    }
-    
-    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
-        
-        return section == 0 ? "Невыполненная задача" : "Выполненная задача"
+        //return defaultTask?[section].count ?? 0
+
+        return sectionInfo?.numberOfObjects ?? 0
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
- 
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.value1, reuseIdentifier: Properties.mainCellId.rawValue)
+        
+//        let task = defaultTask?[indexPath.section][indexPath.row]
+//        cell.textLabel?.text = task?.name ?? "Ничего нет"
+//        cell.imageView?.image = UIImage(named: task?.imageName ?? "Checklist")
 
-        let task = defaultTask?[indexPath.section][indexPath.row]
-        cell.textLabel?.text = task?.name ?? "Ничего нет"
-        cell.imageView?.image = UIImage(named: task?.imageName ?? "Checklist")
+        let task = fetchResultController.object(at: indexPath)
+        
+        cell.textLabel?.text = task.name
         return cell
     }
     
-    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
-        return presenter?.doneCompletedTask(indexPath)
+//    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+//        return presenter?.doneCompletedTask(indexPath)
+//    }
+}
+
+extension MainViewController: NSFetchedResultsControllerDelegate {
+    func controller(_ controller: NSFetchedResultsController<NSFetchRequestResult>, didChange anObject: Any, at indexPath: IndexPath?, for type: NSFetchedResultsChangeType, newIndexPath: IndexPath?) {
+        
+        switch type {
+        case .insert:
+            
+            if indexPath != nil {
+                tableView.insertRows(at: [indexPath!], with: .automatic)
+            }
+        default:
+            break
+        }
     }
 }
+
